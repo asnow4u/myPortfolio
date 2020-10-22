774bf8b2e55a724e0d66d884baeda9fa44db7d12
asnow4u <asnow4u@gmail.com>
2020-10-22 16:03:42 -0600

    Added functionality to the navBar. Now changes color when hovered over and routes correctly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Switch, Route} from 'react-router-dom';
 import ProfilePicture from './components/ProfilePicture';
 import Navbar from './components/Navbar';
 import ProfileInfo from './components/ProfileInfo';
@@ -11,14 +12,27 @@ function App() {
   return (
     <Container fluid>
       <Row>
-        <Col xs={5}>
-          <ProfilePicture />
-        </Col>
-        <Col>
-          <div className="profile">
-            <ProfileInfo />
-          </div>
-        </Col>
+        <Switch>
+          <Route path = "/projects">
+
+          </Route>
+
+          <Route path = "/contact">
+
+          </Route>
+
+          <Route path="/">
+            <Col xs={5}>
+              <ProfilePicture />
+            </Col>
+            <Col>
+              <div className="profile">
+                <ProfileInfo />
+              </div>
+            </Col>
+          </Route>
+
+        </Switch>
         <Col xs={1}>
           <div className="navBar">
             <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import '../style/navbar.css';
 
 
@@ -13,62 +14,143 @@ class Navbar extends React.Component {
         profileHover: false,
         projectHover: false,
         contactHover: false,
-        profileFile: process.env.PUBLIC_URL + '/img/profile.png',
+        profileFile: process.env.PUBLIC_URL + '/img/profile_hover.png',
         projectFile: process.env.PUBLIC_URL + '/img/project.png',
         contactFile: process.env.PUBLIC_URL + '/img/contact.png'
       };
   }
 
+  /*
+    Profile
+    updateNavProfile: Updates navBar when an profile is clicked
+    hoverProfileToggle: Updates profile icon and background when hovered over
+  */
+  updateNavProfile = () => {
+    //Update profile button
+    let button = document.getElementById("profileLink").style.backgroundColor = "black";
+    this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile_hover.png'});
 
-  hoverProfileToggle = () => {
-    this.setState({profileHover: !this.state.profileHover});
+    //Reset project button and icon
+    button = document.getElementById("projectLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({projectFile: process.env.PUBLIC_URL + '/img/project.png'});
 
-    if (this.state.profileHover){
-      this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile.png'});
-    } else {
-      this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile_hover.png'});
+    //Reset contact button and icon
+    button = document.getElementById("contactLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact.png'});
+  }
+
+  hoverProfileToggle = (hover) =>{
+    const urlString = window.location.pathname;
+    if (urlString == '/projects' || urlString == '/contact'){
+
+      if (hover){
+        this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile_hover.png'});
+        let button = document.getElementById("profileLink").style.backgroundColor = "black";
+
+      } else {
+
+        this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile.png'});
+        let button = document.getElementById("profileLink").style.backgroundColor = "rgb(204, 197, 0)";
+      }
     }
   }
 
-  hoverProjectToggle = () => {
-    this.setState({projectHover: !this.state.projectHover});
 
-    if (this.state.projectHover){
-      this.setState({projectFile: process.env.PUBLIC_URL + '/img/project.png'});
-    } else {
-      this.setState({projectFile: process.env.PUBLIC_URL + '/img/project_hover.png'});
+  /*
+    Project
+    updateNavProject: Updates navBar when an project is clicked
+    hoverProjectToggle: Updates project icon and background when hovered over
+  */
+  updateNavProject = () => {
+    let button = document.getElementById("projectLink").style.backgroundColor = "black";
+    this.setState({projectFile: process.env.PUBLIC_URL + '/img/project_hover.png'});
+
+    //Reset profile button and icon
+    button = document.getElementById("profileLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile.png'});
+
+    //Reset contact button and icon
+    button = document.getElementById("contactLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact.png'});
+  }
+
+  hoverProjectToggle = (hover) => {
+    const urlString = window.location.pathname;
+    if (urlString != '/projects'){
+
+      if (hover){
+        this.setState({projectFile: process.env.PUBLIC_URL + '/img/project_hover.png'});
+        let button = document.getElementById("projectLink").style.backgroundColor = "black";
+
+      } else {
+
+        this.setState({projectFile: process.env.PUBLIC_URL + '/img/project.png'});
+        let button = document.getElementById("projectLink").style.backgroundColor = "rgb(204, 197, 0)";
+      }
     }
   }
 
-  hoverContactToggle = () => {
-    this.setState({contactHover: !this.state.contactHover});
 
-    if (this.state.contactHover){
-      this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact.png'});
-    } else {
-      this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact_hover.png'});
+  /*
+    Contact
+    updateNavContact: Updates navBar when an contact is clicked
+    hoverContactToggle: Updates contact icon and background when hovered over
+  */
+  updateNavContact = () => {
+    let button = document.getElementById("contactLink").style.backgroundColor = "black";
+    this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact_hover.png'});
+
+    //Reset profile button and icon
+    button = document.getElementById("profileLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({profileFile: process.env.PUBLIC_URL + '/img/profile.png'});
+
+    //Reset project button and icon
+    button = document.getElementById("projectLink").style.backgroundColor = "rgb(204, 197, 0)";
+    this.setState({projectFile: process.env.PUBLIC_URL + '/img/project.png'});
+  }
+
+  hoverContactToggle = (hover) => {
+    const urlString = window.location.pathname;
+    if (urlString != '/contact'){
+
+      if (hover){
+        this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact_hover.png'});
+        let button = document.getElementById("contactLink").style.backgroundColor = "black";
+
+      } else {
+
+        this.setState({contactFile: process.env.PUBLIC_URL + '/img/contact.png'});
+        let button = document.getElementById("contactLink").style.backgroundColor = "rgb(204, 197, 0)";
+      }
     }
   }
 
 
   render() {
+
     return (
       <div className="navDiv">
         <Container>
           <Row>
-            <button className="button homeButton" onMouseOver={this.hoverProfileToggle} onMouseOut={this.hoverProfileToggle}>
-              <img className="linkImage" id="homeImage" src={this.state.profileFile}/>
-            </button>
+            <Link to="/" onClick={this.updateNavProfile}>
+              <button className="button homeButton" id="profileLink" onMouseOver={() => this.hoverProfileToggle(true)} onMouseOut={() => this.hoverProfileToggle(false)}>
+                <img className="linkImage" id="homeImage" src={this.state.profileFile}/>
+              </button>
+            </Link>
           </Row>
           <Row>
-            <button className="button projectButton" onMouseOver={this.hoverProjectToggle} onMouseOut={this.hoverProjectToggle}>
-              <img className="linkImage" id="projectImage" src={this.state.projectFile}/>
-            </button>
+            <Link to="/projects" onClick={this.updateNavProject}>
+              <button className="button" id="projectLink" onMouseOver={() => this.hoverProjectToggle(true)} onMouseOut={() => this.hoverProjectToggle(false)}>
+                <img className="linkImage" id="projectImage" src={this.state.projectFile}/>
+              </button>
+            </Link>
           </Row>
           <Row>
-            <button className="button contactButton" onMouseOver={this.hoverContactToggle} onMouseOut={this.hoverContactToggle}>
-              <img className="linkImage" id="contactImage" src={this.state.contactFile}/>
-            </button>
+            <Link to="/contact" onClick={this.updateNavContact}>
+              <button className="button" id="contactLink" onMouseOver={() => this.hoverContactToggle(true)} onMouseOut={() => this.hoverContactToggle(false)}>
+                <img className="linkImage" id="contactImage" src={this.state.contactFile}/>
+              </button>
+            </Link>
           </Row>
         </Container>
       </div>
